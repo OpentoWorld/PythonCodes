7dec7c7031c253cd4097036e4563d562a7baf26d
Rakesh Thakur <56785201+OpentoWorld@users.noreply.github.com>
2019-11-07 01:41:18 +0530

    Add files via upload

---
+++ Functions and Oops/VariableLengthArguments.py
@@ -0,0 +1,7 @@
+def info(user, *users):
+    print("User of Python:", user)
+    for var in users:
+        print("Users of Python:", var)
+        return ()
+info("Rakesh")
+info("Rakesh", "Krishna")
